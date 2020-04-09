4f9dffa15ec09175a4b6a83d44235c1a0df486f9
Nico Mendoza <blankartwp@gmail.com>
2020-04-09 12:04:00 +0800

    fixed devtools extension

--- Client/src/common/configStore.js
+++ Client/src/common/configStore.js
@@ -23,14 +23,16 @@ if (process.env.NODE_ENV === 'development') {
   }
 }
 
+const devTools =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : null;
+
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    compose(
-      applyMiddleware(...middlewares),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
+    compose(applyMiddleware(...middlewares), devToolsExtension),
   );
 
   /* istanbul ignore if  */
