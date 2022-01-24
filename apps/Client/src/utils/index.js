e032608f2774ddbf5346e90e2ddbd17f83656181
ncmendoza01 <n.mendoza@talkinnovations.com>
2022-01-24 19:23:05 +0800

    fix links

--- apps/Client/src/utils/index.js
+++ apps/Client/src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://api-deehwaliongdev.herokuapp.com';
+export const BASE_URL = 'https://api-dhla-web-app.vercel.app';
 export const WEB_BASE_URL = 'https://dhla-web-app.vercel.app';
 export const getImageUrl = imageUrl => {
   return `${BASE_URL}/${imageUrl}`;
